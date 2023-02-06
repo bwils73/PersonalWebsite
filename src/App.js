79c0e9603664319aca2317a858c3876bd229ac32
Brandon <brandon.wilary@gmail.com>
2023-02-05 21:18:21 -0500

    in progress

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Website In Progress
           </p>
          <a
             className="App-link"
