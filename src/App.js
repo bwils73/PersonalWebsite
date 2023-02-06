0ad8f8bc6155c371cefe4edeb5887838b303c03e
Brandon <brandon.wilary@gmail.com>
2023-02-05 20:34:12 -0500

    firebase

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase/compat/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+
+firebase.initializeApp( {
+  apiKey: "AIzaSyAZACcI1lhWDNqadKmaDSzu0HPi0RaQHro",
+  authDomain: "personal-website-6caf2.firebaseapp.com",
+  projectId: "personal-website-6caf2",
+  storageBucket: "personal-website-6caf2.appspot.com",
+  messagingSenderId: "955752158806",
+  appId: "1:955752158806:web:67eea3240ee7a4f1d94df7"
+})
+
+//const auth = firebase.auth();
+//const firestore = firebase.firestore();
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+         <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
           Learn React
         </a>
       </header>
