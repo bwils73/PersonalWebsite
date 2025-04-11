4e09a6558ebff53fd5865e4c3454d643ee22321c
bwils73 <brandon.wilary@gmail.com>
2025-04-11 16:18:51 -0500

    get rid of react bootstrap

---
+++ src/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import {
+    AppBar,
+    Toolbar,
+    CssBaseline,
+    Typography,
+    makeStyles
+} from "@mui/material";
+
+function Navbar() {
+
+    return (
+        <AppBar position="static">
+            <CssBaseline/>
+
+        </AppBar>
+    )
+}
+
+export default Navbar;
